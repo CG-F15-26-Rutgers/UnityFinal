f2d3f22bfda5790c93e0fb73063a3468c5adec58
Anthony Castronuovo <ajc320@scarletmail.rutgers.edu>
2015-12-11 02:31:20 -0500

    Story half done! One more story arc to finish

--- B3/b3/Assets/Scripts/HeroTree.cs
+++ B3/b3/Assets/Scripts/HeroTree.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using TreeSharpPlus;
+using RootMotion.FinalIK;
 
 public class HeroTree : MonoBehaviour {
 
@@ -11,13 +12,26 @@ public class HeroTree : MonoBehaviour {
     public Transform princessTalkPos;
     public Transform oldmanTalkPos;
     public Transform marker;
+    public Transform noteInfoMarker;
+    public Transform pickUpSlab;
+    public Transform lovePoemMarker;
+    public Transform cameraPos;
+
+    public FullBodyBipedEffector Effector;
+    public InteractionObject slab;
+
+    public FullBodyBipedEffector EffectorTwo;
+    public InteractionObject LovePoem;
+
+    public FullBodyBipedEffector EffectorThreePt1;
+    public FullBodyBipedEffector EffectorThreePt2;
+    public InteractionObject giftGive;
 
 
     private BehaviorAgent heroAgent;
 
     private bool princessInfo = false;
     private bool oldManInfo = false;
-
 
 	// Use this for initialization
 	void Start () {
@@ -46,6 +60,7 @@ public class HeroTree : MonoBehaviour {
             this.PrincessWave(),
             this.FirstConversation(),
             this.GoToMarker(),
+            this.Think(),
             this.SelectStoryArc()
             );
     }
@@ -55,9 +70,30 @@ public class HeroTree : MonoBehaviour {
     //
     protected Node LovePoemArc()
     {
-        SelectorShuffle info = new SelectorShuffle(PrincessInfo(), OldGuyInfo());
-        //Sequence checkFirst = new Sequence(new LeafAssert(princessInfo));
-        return info;
+        SequenceShuffle info = new SequenceShuffle(
+            this.PrincessInfo(), 
+            this.OldGuyInfo(), 
+            this.getNoteInfo());
+
+        Sequence cont = new Sequence(
+            this.GoToMarker(),
+            this.Think(), 
+            this.CheckInfo(), 
+            this.Realization());
+
+        Sequence giveItToHer = new Sequence(
+            this.GoToLion(),
+            this.ST_PickUpLovePoem(),
+            this.GoToPrincess(),
+            this.HeroOrientToPrincess(), 
+            new LeafWait(1000),
+            this.PrincessOrientToHero(),
+            new LeafWait(1000),
+            this.GiveGift(),
+            this.Celebrate()
+            );
+
+        return new Sequence(info, cont, giveItToHer);
     }
 
 
@@ -126,6 +162,11 @@ public class HeroTree : MonoBehaviour {
         return new Sequence(Princess.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Wave", 4000));
     }
 
+    protected Node CheckInfo()
+    {
+        return new Sequence(Hero.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Read", 5000));
+    }
+
     protected Node FirstConversation()
     {
         return new Sequence(
@@ -136,6 +177,17 @@ public class HeroTree : MonoBehaviour {
             Hero.GetComponent<BehaviorMecanim>().ST_PlayFaceGesture("HeadNod", 4000),
             Princess.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("Cheer", 4000)
             );
+    }
+
+    protected Node GiveGift()
+    {
+        Val<FullBodyBipedEffector> effectorOne = Val.V(() => EffectorThreePt1);
+        Val<FullBodyBipedEffector> effectorTwo = Val.V(() => EffectorThreePt2);
+        Val<InteractionObject> giftGiving = Val.V(() => giftGive);
+        return new SequenceParallel(new LeafTrace("Give Gift"),
+            Hero.GetComponent<BehaviorMecanim>().Node_StartInteraction(effectorOne, giftGiving),
+            Princess.GetComponent<BehaviorMecanim>().Node_StartInteraction(effectorTwo, giftGiving),
+            new LeafWait(1000));
     }
 
     protected Node GoToMarker()
@@ -162,21 +214,60 @@ public class HeroTree : MonoBehaviour {
             );
     }
 
+    protected Node GoToNoteMarker()
+    {
+        return new Sequence(Hero.GetComponent<BehaviorMecanim>().Node_GoTo(noteInfoMarker.position));
+    }
 
+    protected Node ST_PickUpTablet()
+    {
+        Val<FullBodyBipedEffector> effecting = Val.V(() => Effector);
+        Val<InteractionObject> theThing = Val.V(() => slab);
+        return new SequenceParallel(new LeafTrace("Pick Up Info"), Hero.GetComponent<BehaviorMecanim>().Node_StartInteraction(effecting, theThing), new LeafWait(1000));
+    }
 
+    protected Node ST_PickUpLovePoem()
+    {
+        Val<FullBodyBipedEffector> effecting = Val.V(() => EffectorTwo);
+        Val<InteractionObject> theThingy = Val.V(() => LovePoem);
+        return new SequenceParallel(new LeafTrace("Pick Up Love Poem"), Hero.GetComponent<BehaviorMecanim>().Node_StartInteraction(effecting, theThingy), new LeafWait(1000));
+    }
 
+    protected Node Realization()
+    {
+        return new SequenceShuffle(
+            Hero.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("CrowdPump", 4000),
+            Hero.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("ChestPumpSalute", 4000));
+    }
 
+    protected Node Celebrate()
+    {
+        Val<Vector3> camPos = Val.V(() => cameraPos.position);
 
+        SequenceParallel look = new SequenceParallel(
+            Hero.GetComponent<BehaviorMecanim>().Node_OrientTowards(cameraPos.position),
+            Princess.GetComponent<BehaviorMecanim>().Node_OrientTowards(cameraPos.position));
 
+        SequenceParallel lookEyeCamera = new SequenceParallel(
+            Hero.GetComponent<BehaviorMecanim>().Node_HeadLook(camPos),
+            Princess.GetComponent<BehaviorMecanim>().Node_HeadLook(camPos));
+        
+        SequenceParallel dance = new SequenceParallel(
+            Hero.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("Breakdance", 5000),
+            Princess.GetComponent<BehaviorMecanim>().ST_PlayBodyGesture("Breakdance", 5000));
 
+        SequenceParallel goodbye = new SequenceParallel(
+            this.HeroWave(),
+            this.PrincessWave()
+            );
 
+        return new Sequence(new LeafWait(500), look, new LeafWait(500), lookEyeCamera, new LeafWait(500), dance, new LeafWait(500), goodbye);
+    }
 
-
-
-
-
-
-
+    protected Node GoToLion()
+    {
+        return new Sequence(Hero.GetComponent<BehaviorMecanim>().Node_GoTo(lovePoemMarker.position));
+    }
 
     #endregion
 
@@ -184,7 +275,7 @@ public class HeroTree : MonoBehaviour {
 
     protected Node SelectStoryArc()
     {
-        return new SelectorShuffle(LovePoemArc(), GemArc());
+        return new SelectorShuffle(LovePoemArc());
     }
 
     protected Node PrincessInfo()
@@ -213,6 +304,13 @@ public class HeroTree : MonoBehaviour {
             );
     }
 
+    protected Node getNoteInfo()
+    {
+        return new Sequence(
+            this.GoToNoteMarker(),
+            this.ST_PickUpTablet()
+            );
+    }
     
 
 
